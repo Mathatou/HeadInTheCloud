e91814485ac9fe0ad512f2c7da81d06d2f396b23
Mathatou <130315027+Mathatou@users.noreply.github.com>
2026-04-28 17:22:30 +0200

    modif en l'état

--- HITC/Assets/Scripts/Lobby/LightFire.cs
+++ HITC/Assets/Scripts/Lobby/LightFire.cs
@@ -3,19 +3,23 @@ using UnityEngine;
 public class LightFire : MonoBehaviour
 {
     [SerializeField] private GameObject fire;
-
+    private ParticleSystem ps;
     private void Start()
     {
         if(fire.activeInHierarchy) fire.SetActive(false);
+        ps = GetComponent<ParticleSystem>();
     }
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Lighter"))
         {
-            if(!fire.activeInHierarchy)
-            {
-                fire.SetActive(true);
-            }
+            fire.SetActive(true);
+            ps.Play();
+        }
+        else
+        {
+            Debug.LogWarning("Collided with " + other.name);
+            Debug.LogWarning("Collided with tag : " + other.tag);
         }
     }
 }
