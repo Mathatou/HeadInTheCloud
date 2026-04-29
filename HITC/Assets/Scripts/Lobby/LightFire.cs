4f4e6ee6290d42996e6b60413c508630970103a5
Mathatou <130315027+Mathatou@users.noreply.github.com>
2026-04-29 23:19:19 +0200

    Lobby: add lighter prop and update fire behavior
    Disable particle PlayOnAwake in the LobbyScene, add a new === PROPS === GameObject with a lighter prefab instance (Transform, Rigidbody, BoxCollider modifications) and register it as a scene root. Simplify LightFire.cs to fetch the ParticleSystem from the fire GameObject and play it on trigger (tagged Lighter) instead of toggling the GameObject and logging collisions.

--- HITC/Assets/Scripts/Lobby/LightFire.cs
+++ HITC/Assets/Scripts/Lobby/LightFire.cs
@@ -3,23 +3,14 @@ using UnityEngine;
 public class LightFire : MonoBehaviour
 {
     [SerializeField] private GameObject fire;
-    private ParticleSystem ps;
+    private ParticleSystem fire_ps;
     private void Start()
     {
-        if(fire.activeInHierarchy) fire.SetActive(false);
-        ps = GetComponent<ParticleSystem>();
+        fire_ps = fire.GetComponent<ParticleSystem>();
     }
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Lighter"))
-        {
-            fire.SetActive(true);
-            ps.Play();
-        }
-        else
-        {
-            Debug.LogWarning("Collided with " + other.name);
-            Debug.LogWarning("Collided with tag : " + other.tag);
-        }
+        if(other.CompareTag("Lighter")) fire_ps.Play();
+        
     }
 }
