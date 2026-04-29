128d100c96f79e0caa3f3a379b32f79ead6607cd
Mathatou <130315027+Mathatou@users.noreply.github.com>
2026-04-29 23:47:47 +0200

    normalement le feu ne s'allume qu'avec le briquet qui brule

--- HITC/Assets/Scripts/Lobby/LightFire.cs
+++ HITC/Assets/Scripts/Lobby/LightFire.cs
@@ -4,13 +4,14 @@ public class LightFire : MonoBehaviour
 {
     [SerializeField] private GameObject fire;
     private ParticleSystem fire_ps;
+    [SerializeField] private ParticleSystem fireOfLighter;
     private void Start()
     {
         fire_ps = fire.GetComponent<ParticleSystem>();
     }
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Lighter")) fire_ps.Play();
+        if(other.CompareTag("Lighter") && fireOfLighter.isPlaying) fire_ps.Play();
         
     }
 }
