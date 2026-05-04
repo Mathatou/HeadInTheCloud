14a9136f6cc9dd69c73eb66169a8ef52f5a55c48
Samy MELIANI <melianis@lan.esiee.fr>
2026-05-04 14:58:59 +0200

    Scripts add + Colliders on key

--- HITC/Assets/Scripts/Labyrinth/Walta.cs
+++ HITC/Assets/Scripts/Labyrinth/Walta.cs
@@ -5,10 +5,32 @@ using UnityEngine;
 public class Collectible : MonoBehaviour
 {
 
-    // Update is called once per frame
+    // On crée une référence pour trouver le GameManager
+    [SerializeField] private Cle gameManager;
+
+    void Start()
+    {
+        // On cherche l'objet GameManager dans la scène au lancement
+        gameManager = Object.FindFirstObjectByType<Cle>();
+    }
+
     void Update()
     {
+        // Ton animation de flottement actuelle
         transform.Translate(Vector3.up * Mathf.Sin(Time.time) / 10000);
     }
 
+    // Détection du contact
+    private void OnTriggerEnter(Collider other)
+    {
+        // On vérifie si c'est le joueur qui touche (par son Tag)
+        if (other.CompareTag("Main"))
+        {
+            // On prévient le manager
+            gameManager.AjouterSphere();
+
+            // On détruit la sphère
+            Destroy(gameObject);
+        }
+    }
 }
